ad8bdb362057b6fa98bc0dac63d5e28c53adb5dc
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-07 17:05:15 +0530

    Update account-details.component.ts

--- src/app/components/account-details/account-details.component.ts
+++ src/app/components/account-details/account-details.component.ts
@@ -9,8 +9,6 @@ import { Account } from '../../models/account.model';
 export class AccountDetailsComponent implements OnInit {
   account: Account | null = null;
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
